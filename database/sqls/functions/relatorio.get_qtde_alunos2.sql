1782d40fb0896085d35c923a26dab5cf1e21fce1
williams <williamsalcantara@gmail.com>
2025-07-11 18:40:47 -0300

    corrigindo funcao de contar alunos

--- database/sqls/functions/relatorio.get_qtde_alunos2.sql
+++ database/sqls/functions/relatorio.get_qtde_alunos2.sql
@@ -1,9 +1,12 @@
 CREATE OR REPLACE FUNCTION relatorio.get_qtde_alunos2(integer, integer, integer, integer) RETURNS bigint
     LANGUAGE sql
 AS $_$
-SELECT COUNT(*)
+SELECT COUNT(DISTINCT matricula.cod_matricula)
 FROM pmieducar.matricula
+INNER JOIN pmieducar.matricula_turma ON matricula_turma.ref_cod_matricula = matricula.cod_matricula
 WHERE matricula.ativo = 1
+  AND matricula.aprovado <> 5 -- Não contabiliza reclassificados
+  AND COALESCE(matricula_turma.remanejado, false) = false
   AND (CASE WHEN 0 = $1 THEN TRUE ELSE matricula.ano = $1 END)
   AND (CASE WHEN 0 = $2 THEN TRUE ELSE matricula.ref_ref_cod_escola = $2 END)
   AND (CASE WHEN 0 = $3 THEN TRUE ELSE matricula.ref_cod_curso = $3 END)
