f4405b87790c91be8f5dbeb922e4b93abd0d703d
williams <williamsalcantara@gmail.com>
2026-04-14 01:35:54 -0300

    criando diretorios de cache

--- docker/php/entrypoint.prod.sh
+++ docker/php/entrypoint.prod.sh
@@ -4,7 +4,12 @@ set -e
 cd /var/www/ieducar
 
 # Garante diretorios de escrita do Laravel.
-mkdir -p storage bootstrap/cache
+mkdir -p \
+  storage/logs \
+  storage/framework/cache \
+  storage/framework/sessions \
+  storage/framework/views \
+  bootstrap/cache
 chown -R www-data:www-data storage bootstrap/cache || true
 chmod -R ug+rwX storage bootstrap/cache || true
 
