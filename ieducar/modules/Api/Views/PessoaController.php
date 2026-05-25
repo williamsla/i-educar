0a56c28dee3aba317e4178fbf55f7d26960bfc6e
williams <williamsalcantara@gmail.com>
2026-05-25 12:08:33 -0300

    editando nome social

--- ieducar/modules/Api/Views/PessoaController.php
+++ ieducar/modules/Api/Views/PessoaController.php
@@ -539,7 +539,7 @@ class PessoaController extends ApiCoreController
         $individual->nacionalidade = $this->getRequest()->tipo_nacionalidade ?: $individual->nacionalidade;
         $individual->zona_localizacao_censo = $this->getRequest()->zona_localizacao_censo ?: $individual->zona_localizacao_censo;
         $individual->localizacao_diferenciada = $this->getRequest()->localizacao_diferenciada ?: $individual->localizacao_diferenciada;
-        $individual->nome_social = $this->getRequest()->nome_social ?: $individual->nome_social;
+        $individual->nome_social = $this->getRequest()->nome_social !== null ? $this->getRequest()->nome_social : $individual->nome_social;
         $individual->observacao = $this->getRequest()->observacao_aluno ?: $individual->observacao;
         $individual->sus = $this->getRequest()->sus ?: $individual->sus;
         $individual->nis_pis_pasep = $this->getRequest()->nis_pis_pasep ?: $individual->nis_pis_pasep;
